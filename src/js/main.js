c678aec3d7c9a8774457ab09ac74705fbdee2e8b
unknown <boris.galac@gmail.com>
2024-09-11 16:30:34 +0200

    asd

--- src/js/main.js
+++ src/js/main.js
@@ -1,40 +1,73 @@
-///// HAM BTN AND NAV OPEN/CLOSEDS
+if (!document.querySelector(".main").classList.contains("gallery-main")) {
+  ///// HAM BTN AND NAV OPEN/CLOSEDS
 
-const hamBtn = document.querySelector(".ham-btn");
-const nav = document.querySelector(".nav");
-hamBtn.addEventListener("click", (e) => {
-  if (nav.getAttribute("aria-expanded") === "false") {
-    nav.setAttribute("aria-expanded", "true");
-    hamBtn.setAttribute("data-active", "true");
-  } else {
-    nav.setAttribute("aria-expanded", "false");
-    hamBtn.setAttribute("data-active", "false");
-  }
-  nav.addEventListener("click", (e) => {
-    if (!e.target.matches(".nav__list")) {
+  const hamBtn = document.querySelector(".ham-btn");
+  const nav = document.querySelector(".nav");
+  hamBtn.addEventListener("click", (e) => {
+    if (nav.getAttribute("aria-expanded") === "false") {
+      nav.setAttribute("aria-expanded", "true");
+      hamBtn.setAttribute("data-active", "true");
+    } else {
       nav.setAttribute("aria-expanded", "false");
       hamBtn.setAttribute("data-active", "false");
     }
+    nav.addEventListener("click", (e) => {
+      if (!e.target.matches(".nav__list")) {
+        nav.setAttribute("aria-expanded", "false");
+        hamBtn.setAttribute("data-active", "false");
+      }
+    });
   });
-});
 
-// swiper testimonials cards
+  // swiper testimonials cards
 
-var swiper = new Swiper(".testimonials__wrapper", {
-  slidesPerView: 1,
-  spaceBetween: 10,
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-  breakpoints: {
-    640: {
-      slidesPerView: 2,
-      spaceBetween: 20,
+  var swiper = new Swiper(".testimonials__wrapper", {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
     },
-    1024: {
-      slidesPerView: 2,
-      spaceBetween: 50,
+    breakpoints: {
+      640: {
+        slidesPerView: 2,
+        spaceBetween: 20,
+      },
+      1024: {
+        slidesPerView: 2,
+        spaceBetween: 50,
+      },
     },
+  });
+} else {
+  // baguettebox
+
+  baguetteBox.run(".gallery");
+}
+
+///////// INTERSECTION OBSERVER
+
+// from left stagger
+
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("active");
+      }
+      //  else {
+      //   entry.target.classList.remove("active");
+      // }
+    });
   },
-});
+  { threshold: 0.2 }
+);
+// left
+const left = document.querySelectorAll(".hidden-left");
+left.forEach((el) => observer.observe(el));
+// right
+const right = document.querySelectorAll(".hidden-right");
+right.forEach((el) => observer.observe(el));
+// bottom
+const fade = document.querySelectorAll(".hidden-fade");
+fade.forEach((el) => observer.observe(el));
